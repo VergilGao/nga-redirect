ff98a19fb815b702c4334ad6bcbdaec1c32b4e09
羽先生 <8655163+VergilGao@users.noreply.github.com>
2022-08-30 16:14:49 +0800

    替换为 nga.178.com

--- nga-redirect.user.js
+++ nga-redirect.user.js
@@ -3,17 +3,15 @@
 // @description  重定向NGA玩家社区的各种不同域名的网址到 bbs.nga.cn
 // @updateURL       https://raw.githubusercontent.com/VergilGao/nga-redirect/master/nga-redirect.user.js
 // @downloadURL     https://raw.githubusercontent.com/VergilGao/nga-redirect/master/nga-redirect.user.js
-// @version      1.0.0
+// @version      1.1.0
 // @author       羽先生(VergilGao), 咕德 @ WoW-玛洛加尔-<蓝丨图>
 // @licence      MIT
 // @namespace    https://github.com/VergilGao/nga-redirect
-// @match        *://nga.178.com/*
+// @match        *://bbs.nga.cn/*
 // @match        *://ngabbs.com/*
 // @match        *://g.nga.cn/*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
-document.location.href = document.location.href.replace('nga.178.com', 'bbs.nga.cn')
-                                               .replace('ngabbs.com', 'bbs.nga.cn')
-                                               .replace('g.nga.cn', 'bbs.nga.cn');
+window.location.replace(location.href.replace(location.hostname, 'nga.178.com'));
